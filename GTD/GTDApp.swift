5740fc56d6d28d635134f4590613da4815f31a96
Sergey <121692119+MSergey95@users.noreply.github.com>
2025-02-16 10:06:07 +0800

    Initial Commit

---
+++ GTD/GTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  GTDApp.swift
+//  GTD
+//
+//  Created by Сергей Минеев on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct GTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
